4070ba8ae802c0db438f5bb8a311741bb870d8ce
Ayush Kumar <ayush@criodo.com>
2019-06-19 18:03:34 +0530

    Adding github page

---
+++ precache-manifest.bd5f5b8ec42f72ff1bed8e0d2730555a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5597afe932693af5893550662a6daf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe74db89b375e7c8eed",
+    "url": "/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "6ed18e88b702f7488003",
+    "url": "/static/js/2.d26184c3.chunk.js"
+  },
+  {
+    "revision": "7fe74db89b375e7c8eed",
+    "url": "/static/js/main.307d01b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a8300610a2bc28543f83c763c5aae5c5",
+    "url": "/static/media/app-screen-demo.a8300610.png"
+  }
+]);
